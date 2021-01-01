e7fefdcb5c08af0ba3d68f8489479d61afcb1aa8
alanloret <alan.loret@ensae.fr>
2021-01-01 02:04:00 +0100

    First Pricer for Vanilla Put and Call Options

--- Pricer/vanilla.h
+++ Pricer/vanilla.h
@@ -7,28 +7,7 @@
 
 #include <algorithm>    // Needed for the "max" function
 #include <cmath>
-
-// A simple implementation of the Box-Muller algorithm, used to generate
-// gaussian random numbers - necessary for the Monte Carlo method below
-// Note that C++11 actually provides std::normal_distribution<> in
-// the <random> library, which can be used instead of this function
-
-double gaussian_box_muller() {
-    double x = 0.0;
-    double y = 0.0;
-    double euclid_sq = 0.0;
-
-    // Continue generating two uniform random variables
-    // until the square of their "euclidean distance"
-    // is less than unity
-    do {
-        x = 2.0 * rand() / static_cast<double>(RAND_MAX)-1;
-        y = 2.0 * rand() / static_cast<double>(RAND_MAX)-1;
-        euclid_sq = x*x + y*y;
-    } while (euclid_sq >= 1.0);
-
-    return x*sqrt(-2*log(euclid_sq)/euclid_sq);
-}
+#include"tools.h"
 
 // Pricing a European vanilla call option with a Monte Carlo method
 double monte_carlo_call_price(const int& num_sims, const double& S, const double& K, const double& r, const double& v, const double& T) {
